80da45485eebb1a8d2ee82dd68cf206a58044748
ericvadim <ericvadim@mail.com>
2017-03-07 08:56:14 +0100

    done menu

--- src/app/controllers/main.controller.js
+++ src/app/controllers/main.controller.js
@@ -9,6 +9,14 @@
     function MainController() {
         var vm = this;
         vm.navbarCollapsed = false;
-
+        vm.menus = [
+            { label: 'Home', icon: 'home'},
+            { label: 'Club / League Information', icon: 'th-large'},
+            { label: 'Team(s)', icon: 'th'},
+            { label: 'Players', icon: 'list'},
+            { label: 'Fields', icon: 'headphones'},
+            { label: 'matches', icon: 'tasks'},
+            { label: 'Statistics', icon: 'stats'}
+        ];
     }
 })();
